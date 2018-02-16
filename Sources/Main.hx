c193b1a16804e3df3a73def635185733985dc945
Robert Konrad <git2016@robdangero.us>
2018-02-16 23:14:01 +0100

    Use the Base2D sample

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -1,101 +1,91 @@
 class Main extends hxd.App {
 
-	var bclone : h2d.Bitmap;
-	var texture : h3d.mat.Texture;
-	var pg : h2d.Graphics;
+	var spr : h2d.Sprite;
+	var tf : h2d.Text;
 
 	override function init() {
-		var g = new h2d.Graphics(s2d);
-		g.beginFill(0xFF0000);
-		g.drawRect(10, 10, 100, 100);
-		g.beginFill(0x00FF00, 0.5);
-		g.lineStyle(1, 0xFF00FF);
-		g.drawCircle(100, 100, 30);
-		g.endFill();
+		// creates a new sprite and put it at the center of the sceen
+		spr = new h2d.Sprite(s2d);
+		spr.x = Std.int(s2d.width / 2);
+		spr.y = Std.int(s2d.height / 2);
 
-		// check pie + draw texture
+		// load the haxe logo png into a tile
+		var tile = hxd.Res.hxlogo.toTile();
 
-		var g = new h2d.Graphics(s2d);
-		var bmp = new hxd.BitmapData(64, 64);
-		for( x in 0...64 )
-			for( y in 0...64 )
-				bmp.setPixel(x, y, 0xFF000000 | (x * 4) | ((y * 4) << 8));
-		var tile = h2d.Tile.fromBitmap(bmp);
-		bmp.dispose();
-		g.lineStyle();
+		// change its pivot so it is centered
+		tile = tile.center();
 
-		g.beginTileFill(-32,-32,tile);
-		g.drawPie(0, 0, 32, Math.PI / 3, Math.PI * 5 / 4);
-		g.endFill();
+		for( i in 0...15 ) {
+			// creates a bitmap into the sprite
+			var bmp = new h2d.Bitmap(tile, spr);
 
-		g.beginTileFill(100, -64, 2, 2, tile);
-		g.drawRect(100, -64, 128, 128);
-		g.endFill();
+			// move its position
+			bmp.x = Math.cos(i * Math.PI / 8) * 100;
+			bmp.y = Math.sin(i * Math.PI / 8) * 100;
 
-		g.x = 200;
-		g.y = 100;
+			// makes it transparent by 10%
+			bmp.alpha = 0.1;
 
-		// check the size and alignment of scaled bitmaps
+			// makes the colors adds to the background
+			bmp.blendMode = Add;
+		}
 
-		var bmp = new hxd.BitmapData(256, 256);
-		bmp.clear(0xFFFF00FF);
-		bmp.fill(19, 21, 13, 15, 0xFF202020);
-		bmp.fill(19, 20, 13, 1, 0xFFFF0000);
-		bmp.fill(18, 21, 1, 15, 0xFF00FF00);
-		bmp.fill(19+13, 21, 1, 15, 0xFF0000FF);
-		bmp.fill(19, 21 + 15, 13, 1, 0xFF00FFFF);
-		var tile = h2d.Tile.fromBitmap(bmp);
+		#if (lime || !(cpp || hl))
+		// load a true type font, can be not very high quality
+		var font = hxd.Res.trueTypeFont.build(64);
 
-		bmp.dispose();
+		// creates a text display with the given font
+		tf = new h2d.Text(font, s2d);
 
-		var b = new h2d.Bitmap(tile.sub(19, 21, 13, 15), s2d);
-		b.x = 200;
-		b.y = 200;
-		b.scale(32);
+		// set the text color
+		tf.textColor = 0xFFFFFF;
 
-		var b = new h2d.Bitmap(tile.sub(18, 20, 15, 17), s2d);
-		b.x = 300;
-		b.y = 300;
-		b.scale(13);
+		// adds a red shadow
+		tf.dropShadow = { dx : 3, dy : 3, color : 0xFF0000, alpha : 0.8 };
 
-		// check drawTo texture
+		// set the text color
+		tf.text = "Héllò h2d !";
 
-		texture = new h3d.mat.Texture(256, 256,[Target]);
-		var b = new h2d.Bitmap(h2d.Tile.fromTexture(texture), s2d);
-		b.y = 256;
+		// set the text position
+		tf.x = 20;
+		tf.y = s2d.height - 80;
 
-		// test capture bitmap
+		#end
 
-		bclone = new h2d.Bitmap(h2d.Tile.fromTexture(new h3d.mat.Texture(256, 256)), s2d);
-		bclone.y = 512;
+		// load a bitmap font Resource
+		var font = hxd.Res.customFont.toFont();
 
-		// set up graphics instance for use in redraw()
+		// creates another text field with this font
+		var tf = new h2d.Text(font, s2d);
+		tf.textColor = 0xFFFFFF;
+		tf.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
+		tf.text = "Héllò h2d !";
 
-		pg = new h2d.Graphics();
-		pg.filter = new h2d.filter.Blur(2,2,10);
-		pg.beginFill(0xFF8040, 0.8);
+		tf.y = 20;
+		tf.x = 20;
+		tf.scale(7);
 	}
 
-	function redraw(t:h3d.mat.Texture) {
-		pg.clear();
-		for( i in 0...100 ) {
-			var r = (0.1 + Math.random()) * 10;
-			var s = Math.random() * Math.PI * 2;
-			var a = Math.random() * Math.PI * 2;
-			pg.drawPie(Math.random() * 256, Math.random() * 256, r, s, a);
-		}
-		pg.drawTo(t);
+	// if we the window has been resized
+	override function onResize() {
 
-		var pix = t.capturePixels();
-		bclone.tile.getTexture().uploadPixels(pix);
-		pix.dispose();
+		if( spr == null ) return;
+
+		// center our sprite
+		spr.x = Std.int(s2d.width / 2);
+		spr.y = Std.int(s2d.height / 2);
+
+		// move our text up/down accordingly
+		if( tf != null ) tf.y = s2d.height - 80;
 	}
 
 	override function update(dt:Float) {
-		redraw(texture);
+		// rotate our sprite every frame
+		if( spr != null ) spr.rotation += 0.01 * dt;
 	}
 
 	static function main() {
+		hxd.Res.initEmbed();
 		new Main();
 	}
 
--- khafile.js
+++ khafile.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 let project = new Project('Heaps Test');
 project.addAssets('Assets/**');
 project.addSources('Sources');
 project.addLibrary('heaps');
+project.addDefine('resourcesPath=' + path.resolve('Libraries', 'heaps', 'samples', 'res'));
 resolve(project);
